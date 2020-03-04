bc2d9ddd2d3cf689e1e20d9707b656ebce7fb07e
romasvagg <romasvagg@yahoo.gr>
2020-03-04 14:16:21 +0200

    deutero commit me stage chanes

--- MyAppforGIT/Program.cs
+++ MyAppforGIT/Program.cs
@@ -13,6 +13,12 @@ namespace MyAppforGIT
             Console.WriteLine("simera grapsame 100 grammes kodika");
             //tha tis kanoume ena commit
 
+            Console.WriteLine("alles 200 grammes kodika");
+            Console.WriteLine("alles 200 grammes kodika");
+            Console.WriteLine("alles 200 grammes kodika");
+            //tora kanoume ena deutero commit
+            //grafoume ena comment kai Commit All
+
 
 
         }
