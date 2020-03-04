980bef93e7cce541bb3f472aa21f68c2f366473e
romasvagg <romasvagg@yahoo.gr>
2020-03-04 14:09:31 +0200

    Add project files.

---
+++ MyAppforGIT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyAppforGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
