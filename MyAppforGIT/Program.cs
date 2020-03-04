d581af213072e698a8f24e25051a355b6f8e5f6a
romasvagg <romasvagg@yahoo.gr>
2020-03-04 14:13:39 +0200

    to proto commit meta apo 100 grammes kodika

--- MyAppforGIT/Program.cs
+++ MyAppforGIT/Program.cs
@@ -7,6 +7,14 @@ namespace MyAppforGIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            //simera grapsame 100 grammes kodika
+            Console.WriteLine("simera grapsame 100 grammes kodika");
+            Console.WriteLine("simera grapsame 100 grammes kodika");
+            Console.WriteLine("simera grapsame 100 grammes kodika");
+            //tha tis kanoume ena commit
+
+
+
         }
     }
 }
